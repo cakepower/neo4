e98a77401120052d4ce19fd1cd0a6b37e558c1ef
cakepower <32028127+cakepower@users.noreply.github.com>
2021-11-19 09:49:54 +0900

    ring neopixel 불 빛이 전진했다 후진하는 새로운 모듈추가

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+index3 = 0
+index = 0
 strip = neopixel.create(DigitalPin.P13, 24, NeoPixelMode.RGB)
-strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.BLACK))
+strip.set_brightness(20)
+strip.show_rainbow(1, 360)
+strip.show()
+basic.pause(300)
 
 def on_forever():
-    serial.write_number(pins.analog_read_pin(AnalogPin.P11))
-    serial.write_line("")
-    if input.light_level() > 220:
-        strip.rotate(1)
-        basic.pause(500)
+    global index3
+    for index2 in range(24):
+        serial.write_number(index2)
+        serial.write_line("")
+        strip.set_pixel_color(index2, neopixel.colors(NeoPixelColors.BLACK))
         strip.show()
-    elif input.light_level() > 210:
-        strip.show_rainbow(1, 360)
+        basic.pause(300)
+    basic.pause(300)
+    index3 = 23
+    while index3 >= 0:
+        serial.write_number(index3)
+        serial.write_line("")
+        strip.set_pixel_color(index3, neopixel.colors(NeoPixelColors.WHITE))
         strip.show()
-        strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.BLACK))
-    else:
-        strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
-        strip.clear()
-        strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.BLACK))
+        basic.pause(100)
+        index3 = index3 - 1
+    strip.show_rainbow(1, 360)
+    strip.show()
+    basic.pause(300)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,20 +1,29 @@
+let index3 = 0
+let index = 0
 let strip = neopixel.create(DigitalPin.P13, 24, NeoPixelMode.RGB)
-strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+strip.setBrightness(20)
+strip.showRainbow(1, 360)
+strip.show()
+basic.pause(300)
 basic.forever(function () {
-    serial.writeNumber(pins.analogReadPin(AnalogPin.P3))
-    serial.writeLine("")
-    if (pins.analogReadPin(AnalogPin.P3) > 1000) {
-        strip.showColor(neopixel.colors(NeoPixelColors.White))
+    for (let index2 = 0; index2 <= 23; index2++) {
+        serial.writeNumber(index2)
+        serial.writeLine("")
+        strip.setPixelColor(index2, neopixel.colors(NeoPixelColors.Black))
         strip.show()
-        basic.pause(200)
-    } else if (pins.analogReadPin(AnalogPin.P3) > 800) {
-        strip.showRainbow(1, 360)
-        strip.rotate(1)
-        strip.show()
-        basic.pause(200)
-    } else {
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        strip.clear()
-        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        basic.pause(300)
     }
+    basic.pause(300)
+    index3 = 23
+    while (index3 >= 0) {
+        serial.writeNumber(index3)
+        serial.writeLine("")
+        strip.setPixelColor(index3, neopixel.colors(NeoPixelColors.White))
+        strip.show()
+        basic.pause(100)
+        index3 = index3 - 1
+    }
+    strip.showRainbow(1, 360)
+    strip.show()
+    basic.pause(300)
 })
